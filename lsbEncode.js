f8378c739d1a4e1c4397637ffecdc3dcc7df5574
Jason <j.hlavac@comcast.net>
2025-04-23 21:49:21 -0700

    Binary Encode Function

--- lsbEncode.js
+++ lsbEncode.js
@@ -5,7 +5,8 @@ function lsbEncodeHelper(){
 }
 
 function lsbEncode(message, img, canvas){
-    
+    const startString = "lsbSTART";
+    const endString = "lsbEND"
     const fileList = img.files;
     const currFile = fileList[0];
     const reader = new FileReader();
@@ -18,8 +19,27 @@ function lsbEncode(message, img, canvas){
             context.drawImage(img, 0, 0);
             const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
             console.log(imageData);
+            console.log(stringToBinary("test"));
         };
         img.src = e.target.result;
     };
     reader.readAsDataURL(currFile);
+}
+
+// Convert a string to binary (7 bits per character)
+function stringToBinary(inputString){
+    output = "";
+    for(var i = 0; i < inputString.length; i++){
+        output += inputString[i].charCodeAt(0).toString(2);
+    }
+    return(output);
+}
+
+function binaryToString(inputBinary){
+    output = "";
+    const bitsPerChar = 7
+    for(var i = 0; i < inputBinary.length; i+=bitsPerChar){
+        output += inputBinary.slice(i, i+7).charCodeAt(0).toString(10);
+    }
+    return(output);
 }
